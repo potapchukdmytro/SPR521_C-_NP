675a86ae692b3264b2031a8703821b3e8f7a0f5f
potapchukdmytro <traiget@gmail.com>
2026-06-20 13:18:06 +0300

    UDP

---
+++ NP/UpdClient/UdpServer/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UdpServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int port = 5000;
+            UdpClient server = new UdpClient();
+
+            // Сервер отримує всі повідомлення та виводить їх
+            //while (true)
+            //{
+            //    IPEndPoint clientEndP = new IPEndPoint(IPAddress.Any, 0);
+            //    byte[] receviedBytes = server.Receive(ref clientEndP);
+            //    string clientMessage = Encoding.UTF8.GetString(receviedBytes);
+            //    Console.WriteLine($"Client {clientEndP} sent: " + clientMessage);
+            //}
+
+
+            // Сервер відправляє всім повідомлення
+            //while(true)
+            //{
+            //    Console.Write("Enter message: ");
+            //    string serverMessage = Console.ReadLine();
+            //    byte[] messageBytes = Encoding.UTF8.GetBytes(serverMessage);
+
+            //    var broadcastEndP = new IPEndPoint(IPAddress.Broadcast, port);
+            //    server.Send(messageBytes, broadcastEndP);
+            //}
+
+
+
+            // Сервер відправляє зображення
+            while (true)
+            {
+                int packageSize = 1024 * 32;
+
+                //Console.Write("Enter image path: ");
+                //string imagePath = Console.ReadLine();
+                string imagePath = "";
+                Console.WriteLine("Enter for start");
+                Console.ReadLine();
+
+                byte[] imageBytes = File.ReadAllBytes(imagePath);
+                int imageLength = imageBytes.Length;
+                int packageCount = (int)Math.Ceiling((double)imageLength / packageSize);
+                string countStr = packageCount.ToString();
+                byte[] countBytes = Encoding.UTF8.GetBytes(countStr);
+
+                var broadcastEndP = new IPEndPoint(IPAddress.Broadcast, port);
+
+                server.Send(countBytes, broadcastEndP);
+
+                Thread.Sleep(100);
+
+                Console.WriteLine("Package count: " + packageCount);
+                for (int i = 0; i < packageCount; i++)
+                {
+                    server.Send(imageBytes.Skip(i * packageSize).Take(packageSize).ToArray(), broadcastEndP);
+                    Thread.Sleep(1);
+                    double loadProcent = (i + 1) / (double)packageCount * 100;
+                    Console.WriteLine($"{loadProcent:0.0}%");
+                }
+
+                Console.WriteLine("Image sent");
+            }
+        }
+    }
+}
---
+++ NP/UpdClient/UpdClient/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UpdClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int port = 5000;
+            UdpClient udpClient = new UdpClient(port);
+
+            // Відправка
+            //while (true)
+            //{
+            //    Console.Write("Enter text: ");
+            //    string text = Console.ReadLine();
+            //    byte[] bytes = Encoding.UTF8.GetBytes(text);
+
+            //    udpClient.Send(bytes, bytes.Length, serverEndP);
+            //}
+
+            // Отримання
+            //while(true)
+            //{
+            //    IPEndPoint broadcastEndP = new IPEndPoint(IPAddress.Any, port);
+            //    byte[] serverMessage = udpClient.Receive(ref broadcastEndP);
+            //    string message = Encoding.UTF8.GetString(serverMessage);
+            //    Console.WriteLine("Server sent: " + message);
+            //}
+
+
+
+            // Отримання фото
+            while (true)
+            {
+                List<byte> imageBytes = new List<byte>();
+
+                IPEndPoint broadcastEndP = new IPEndPoint(IPAddress.Any, port);
+                byte[] packageCount = udpClient.Receive(ref broadcastEndP);
+                string packagesStr = Encoding.UTF8.GetString(packageCount);
+                bool res = int.TryParse(packagesStr, out int packages);
+
+                if (res)
+                {
+                    for (int i = 0; i < packages; i++)
+                    {
+                        byte[] package = udpClient.Receive(ref broadcastEndP);
+                        imageBytes.AddRange(package);
+                    }
+
+                    File.WriteAllBytes("", imageBytes.ToArray());
+                    Console.WriteLine("Image saved");
+                }
+            }
+        }
+    }
+}
