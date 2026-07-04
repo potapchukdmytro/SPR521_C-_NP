4f40903542b2ae1853a56a6c91786da6845c3df3
potapchukdmytro <traiget@gmail.com>
2026-07-04 11:54:02 +0300

    Patterns

---
+++ C#/Patterns/Patterns/Adapter/Data.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns.Adapter
+{
+    public class Data
+    {
+        public string GetData()
+        {
+            return "<BudgetMax>$1000000</BudgetMax>" +
+                    "<BudgetMin>$600000</BudgetMin>";
+        }
+    }
+}
